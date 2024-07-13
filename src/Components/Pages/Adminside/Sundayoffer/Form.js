213ef469d33fc7db6de167e3681f77c00308b442
FarukhBabar <fahadawan32085346@gmail.com>
2024-07-13 16:53:28 +0500

    froala

--- src/Components/Pages/Adminside/Sundayoffer/Form.js
+++ src/Components/Pages/Adminside/Sundayoffer/Form.js
@@ -4,6 +4,7 @@ import '../admin.css';
 import 'froala-editor/css/froala_style.min.css';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 import FroalaEditorComponent from 'react-froala-wysiwyg';
+import 'froala-editor/js/plugins.pkgd.min.js';
 
 const Form = () => {
     const [Image, setImage] = useState(null);
